39443584464ffc02f187b34a26e992abbd6d97ba
Joush <37282701+RexJoush@users.noreply.github.com>
2021-01-24 15:16:22 +0800

    2021.01.24 后端完成

--- server.js
+++ server.js
@@ -13,33 +13,37 @@ net.createServer((socket) => {
     // 绑定数据事件
     socket.on('data', (data) => {
 
+        /*
+            data 数据格式 Wxxxx=xddddddd
+                用户码: W0.00=oF7VY5IGuEFW5yD-U4tiXFC8po5A
+                商品码: W0.00=c12345678
+         */
         console.log(data.toString());
-        let s = data.toString();
+        let s = data.toString().split("=");
+        let w = s[0].substring(1,s[0].length) * 1;
+        let id = s[1];
 
-        // socket.write("hello esp!");
+        // 商品码，加入当前用户的购物车
+        if (id[1] === 'c'){
+            let openid = 'oF7VY5IGuEFW5yD-U4tiXFC8po5A';
+            let commodity_id = id.substring(1, id.length).trim();
+            mysql.connection.query("select cart from cart where openid = 'oF7VY5IGuEFW5yD-U4tiXFC8po5A'",[],(err, result) => {
+                let sql = "INSERT INTO purchased (commodity_id, cart, 1) VALUES (?,?)";
 
-        // 商品码，生成购物车id
-
-        if (data.toString()[6] === 'u'){
-
-            let commodity_id = s.substring(7, s.length).trim();
-            console.log(commodity_id);
-            let cart_id = uuid.v4();
-            let sql = "INSERT INTO purchased (commodity_id, cart) VALUES (?,?)";
-
-            mysql.connection.query(sql, [commodity_id,cart_id], (err, result) =>{
-                socket.write("=y"); // 返回黄灯，表示加入购物车成功
-            })
+                mysql.connection.query(sql, [commodity_id, result[0].cart], (err, result) =>{
+                    socket.write("y"); // 返回黄灯，表示加入购物车成功
+                })
+            });
 
         }
-        // 用户码
+        // 用户码，生成购物车id
         else {
             let cart_id = uuid.v4();
-            let openid = s.substring(6,s.length).trim();
+            let openid = id.trim();
             console.log(openid);
             let sql = "INSERT INTO cart (openid, cart) VALUES (?,?)";
-            mysql.connection.query(sql, [cart_id, data.toString()], (err, result) => {
-                socket.write("=g"); // 返回绑定成功，绿灯
+            mysql.connection.query(sql, [openid, cart_id], (err, result) => {
+                socket.write("g"); // 返回绑定成功，绿灯
             });
         }
 
